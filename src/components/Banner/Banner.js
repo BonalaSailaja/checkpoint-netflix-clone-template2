7930c234d4d32ab9c6836ddf04d29ff2319db537
Sailaja <121034691+BonalaSailaja@users.noreply.github.com>
2023-03-01 09:45:16 +0530

    changes commited

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,19 +5,23 @@ import requests from "../../request";
 import "./Banner.css";
 
 function Banner() {
-  const [series, setSeries] = useState([]);
+  const [movie, setMovie] = useState([]);
   useEffect(() => {
-    let getData = async () => {
+    async function fetchData(){
       let request = await axios.get(requests.fetchNetflixOriginals);
       // console.log(request);
-      setSeries(
+      setMovie(
         request.data.results[
           Math.floor(Math.random() * request.data.results.length)
         ]
       );
     };
-    getData();
+    fetchData();
   }, []);
+
+  function truncate(str,n){
+    return str?.length>n ?str.substr(0,n-1)+"...":str;
+  }
 
   console.log();
   return (
@@ -25,19 +29,19 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(" https://image.tmdb.org/t/p/original/${series?.backdrop_path}")`,
+        backgroundImage: `url(" https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "50% 10%",
       }}
     >
       <div className="banner__contents">
         <h1 className="banner__title">
-          {series?.title || series.name || series?.original_name}
+          {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{series?.overview}</h1>
+        <h1 className="banner__description">{truncate(movie?.overview,150)}</h1>
       </div>
       <div className="banner__fadeBottom" />
     </header>
